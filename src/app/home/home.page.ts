9b6ec78f3a21e39bb9a7daac4eaf88f330d0e63a
Fabian Kalweit <fabiankalweit@yahoo.de>
2020-01-24 12:25:49 +0100

    first Version apc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  aperture:number;
+  pixelPitch:number;
+  focalLength:number;
+  shutterTime:number;
 
+  constructor() {
+
+    // this.aperture = 1;
+    // this.pixelPitch = 1;
+    // this.focalLength  = 1;
+    // this.shutterTime = 1;
+
+  }
+
+  calcTime(){
+    console.log("huhu");
+    console.log(this.aperture);
+    console.log(this.pixelPitch);
+    console.log(this.focalLength);
+
+    //{(35 x Aperture) + (30 x pixel pitch)} ÷ focal length
+    this.shutterTime = ((35 * this.aperture) + (30 * this.pixelPitch)) / this.focalLength
+    
+    console.log(this.shutterTime);
+  }
 }
